7f586ab3910f78f37b2b3a573e573d1b71633979
GammaCinders <talsmake@mail.edu.gvsu>
2022-10-18 17:42:01 -0400

    Moved validate functionality and created set(sort) method

--- Assignment8/heap.py
+++ Assignment8/heap.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass;
 from graphics import *;
-import math;
+import random; 
+import time;
 
 @dataclass
 class HeapObject(object):
@@ -23,7 +24,7 @@ class Heap(object):
 
     def insert(self, data) -> None:
         #if full can't insert
-        if(self.size == len(self.heap)):
+        if(self.size == self.n):
            return;
 
         self.heap[self.size] = data;
@@ -32,32 +33,33 @@ class Heap(object):
         #if root nothing else to do
         if(self.size == 1):
             return;
-
-        newIndex = self.size;
-        parentIndex = int(newIndex / 2);
-        while(self.heap[newIndex-1] > self.heap[parentIndex-1]):
-            temp = self.heap[newIndex-1];
-            self.heap[newIndex-1] = self.heap[parentIndex-1];
-            self.heap[parentIndex-1] = temp;
-
-            newIndex = parentIndex;
-            parentIndex = int(parentIndex / 2);
-            if(parentIndex == 0):
-                return;
+        self.validate(self.size);
 
     def setHeap(self, heap: list) -> None:
         self.heap = heap;
         self.size = len(heap);
-        self.validate();
+        for i in range(1, self.size+1):
+            self.validate(i);
 
-    def validate(self) -> bool:
+    def validate(self, i: int) -> bool:
+        parentIndex = int(i / 2);
+        while(self.heap[i-1] > self.heap[parentIndex-1]):
+            temp = self.heap[i-1];
+            self.heap[i-1] = self.heap[parentIndex-1];
+            self.heap[parentIndex-1] = temp;
+
+            i = parentIndex;
+            parentIndex = int(parentIndex / 2);
+            if(parentIndex == 0):
+                return;
+
         return True;
 
     def print(self, title) -> GraphWin:
-        win = GraphWin(title, 1200, 1000);
+        win = GraphWin(title, 1400, 1000);
         win.setBackground(color_rgb(40, 40, 40));
 
-        self.printNodes(win, 1, Point(0, 0), 600);
+        self.printNodes(win, 1, Point(0, 0), 700);
 
         return win;
 
@@ -93,21 +95,57 @@ class Heap(object):
         dataText.setOutline("white");
         dataText.draw(win);
 
-#Random numbers (from Random.org, 100 integers from 1 to 250, sorted)
-data = {
-        1, 3, 4, 7, 8, 11, 14, 15, 18, 19, 21, 23, 26, 30, 31, 32,
-        34, 42, 43, 45, 46, 50, 51, 52, 53, 54, 57, 59, 61, 62, 65,
-        68, 72, 75, 76, 77, 81, 82, 84, 87, 89, 91, 93, 97, 98, 103,
-        105, 106, 112, 117, 119, 124, 128, 129, 131, 132, 133, 134,
-        137, 138, 142, 143, 150, 151, 153, 156, 161, 165, 166, 174, 177,
-        178, 180, 181, 183, 184, 185, 187, 193, 198, 202, 203, 206, 207, 
-        215, 216, 222, 223, 228, 231, 232, 238, 241, 242, 245, 246, 247, 
-        248, 249, 250
-}
 
+########################################
+# Testing speeds for insert methods 
+# and list sorts
+########################################
 
-heapOneByOne = Heap(100);
+data = [];
+for i in range(1, 1001):
+    data.append(i);
+
+########################################
+# In ascending order
+########################################
+
+heapOneByOne = Heap(1000);
+pre = time.time();
 for i in data:
     heapOneByOne.insert(i); 
-heapOneByOne.print("Test").getKey();
+print(time.time() - pre);
 
+pre = time.time();
+heapAtOnce = Heap(1000);
+heapAtOnce.setHeap(data);
+print(time.time() - pre);
+
+########################################
+# In descending order
+########################################
+
+heapOneByOne = Heap(1000);
+pre = time.time();
+for i in data:
+    heapOneByOne.insert(i); 
+print(time.time() - pre);
+
+pre = time.time();
+heapAtOnce = Heap(1000);
+heapAtOnce.setHeap(data);
+print(time.time() - pre);
+
+########################################
+# In random order 
+########################################
+
+heapOneByOne = Heap(1000);
+pre = time.time();
+for i in data:
+    heapOneByOne.insert(i); 
+print(time.time() - pre);
+
+pre = time.time();
+heapAtOnce = Heap(1000);
+heapAtOnce.setHeap(data);
+print(time.time() - pre);
