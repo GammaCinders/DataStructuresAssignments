d85d758fc2bafb63982afa24c1bc75476e37b5c8
GammaCinders <talsmake@mail.edu.gvsu>
2022-11-17 13:44:04 -0500

    Updated assignment 12

--- Assignment12/Knapsack.py
+++ Assignment12/Knapsack.py
@@ -11,3 +11,40 @@ class Item(object):
 
     def __str__(self):
         return f"Item: ({self.weight}, {self.value})";
+
+class House(object):
+    items: list;
+
+    def __init__(self):
+        self.items = [];
+
+    def addItem(self, weight: int, value: int):
+        self.items.append(Item(weight, value));
+
+    def findMaxValue(self, maxWeight: int):
+        maxValue = [[0]*len(self.items) for i in range(len(self.items))];
+
+        for item in range(len(self.items)):
+            for maxWeight in range(len(self.items[item])):
+                # Need temp weight and value
+                if (self.items[item].weight <= maxWeight):
+                pass;
+
+        # Go through each row
+        # Go through each column
+
+    def __str__(self):
+        out = "";
+        for item in self.items:
+            out += f"{item}\n";
+        return out;
+
+
+
+firstSet = [(1, 1), (6, 2), (18, 5), (22, 6), (28, 7)];
+
+house = House();
+for item in firstSet:
+    house.addItem(item[0], item[1]);
+house.findMaxValue(100);
+print(house);
