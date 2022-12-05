ad6a18a07c9feccf0ea1ea7b89dfa9b666b4712a
Keagen Talsma <keegenjt@gmail.com>
2022-12-04 22:11:34 -0500

    Added test output
    Added some simple test outputs necessary for assignment.

--- Assignment13/Partition.py
+++ Assignment13/Partition.py
@@ -1,4 +1,9 @@
 
+########################################
+# Solution code for the partition
+# problem
+########################################
+
 # This just checks if a partition is even possible
 # (aka if the sum / 2 is even)
 def partition(array: list) -> bool:
@@ -22,5 +27,34 @@ def partitionHelper(array: list, target: int) -> bool:
     
     return False;
 
-test = [1, 5, 5, 4, 6, 10, 1, 2, 2];
-print(partition(test));
+
+
+########################################
+# Testing code
+########################################
+
+print();
+
+# This has a valid partition: 10 + 6 + 2 = 18 = half sum
+# (this is caught in 'partitionHelper()')
+partitionExists = [1, 5, 5, 4, 6, 10, 1, 2, 2];
+print(f"Array = {partitionExists}")
+print(f"Partition does exist: {partition(partitionExists)}");
+print(f"Half sum = {sum(partitionExists)/2}");
+print();
+
+# This cannot have a partition because sum is odd, so you can't
+# have 2 even integer halfs (this is caught in 'partition()')
+partitionNotPossible = [32, 30, 11, 3, 49, 84];
+print(f"Array = {partitionNotPossible}")
+print(f"Partition does exist: {partition(partitionNotPossible)}");
+print(f"Half sum = {sum(partitionNotPossible)/2}");
+print();
+
+# This could have a partition, but there is no way to get the half sum
+# (this is caught in 'partitionHelper()')
+partitionDoesNotExist = [20, 10, 14, 3, 7, 4];
+print(f"Array = {partitionDoesNotExist}")
+print(f"Partition does exist: {partition(partitionDoesNotExist)}");
+print(f"Half sum = {sum(partitionDoesNotExist)/2}");
+print();
